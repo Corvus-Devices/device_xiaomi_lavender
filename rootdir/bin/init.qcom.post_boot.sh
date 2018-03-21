d37d0190861eca218aa241637645fc0b9fe7bdfc
Tim Murray <timmurray@google.com>
2018-03-21 11:13:57 -0700

    lavender: rootdir: disable mem_latency governor
    Unclear if advantages are worth cumulative power draw.
    
    bug 73364974
    Test: boot taimen, memlat is powersave
    
    Change-Id: I7b5eb176d0f22281e687b9577f5eedf311fb4a11

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -355,13 +355,6 @@ case "$target" in
     echo 1600 > $cpubw/bw_hwmon/idle_mbps
     done
 
-    for memlat in /sys/class/devfreq/*qcom,memlat-cpu*
-    do
-    echo "mem_latency" > $memlat/governor
-    echo 10 > $memlat/polling_interval
-    echo 400 > $memlat/mem_latency/ratio_ceil
-    done
-
     echo "cpufreq" > /sys/class/devfreq/soc:qcom,mincpubw/governor
 
     echo "15360,19200,23040,26880,34415,43737" > /sys/module/lowmemorykiller/parameters/minfree
