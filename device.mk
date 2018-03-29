a76ace57fa6b668ac76e9b2410d3d8308b6e8bf0
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-03-29 20:45:23 +0200

    lavender: build vendor.qti.hardware.camera.device for ims
    Change-Id: I736e19ff5ded1c493d61f6ed400c6decab318c8c
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- device.mk
+++ device.mk
@@ -139,6 +139,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
     Snap
 
 # Codec2 modules
