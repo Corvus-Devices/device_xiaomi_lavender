f1e7af7f0d3a6c65ef16a2955f13a1517c547d8d
Ethan Chen <intervigil@gmail.com>
2018-04-02 22:38:42 -0700

    lavender: Remove init.qcom.early_boot.sh
    * This script doesn't do anything useful, since it tries to set a lot of
      ro.* properties, which is not allowed anyway.
    
    Change-Id: I408cc17d0d18e81a9cf0e529e6b78622c3017a7f
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- device.mk
+++ device.mk
@@ -349,7 +349,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
-    init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.usb.sh \
     init.qcom.sensors.sh \
