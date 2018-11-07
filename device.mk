bb7ff4fbb832ee1d2aaa49c6cc539c386ec4ca25
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    lavender: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- device.mk
+++ device.mk
@@ -16,9 +16,7 @@ DEVICE_PACKAGE_OVERLAYS := \
     $(LOCAL_PATH)/overlay \
 #    $(LOCAL_PATH)/overlay-aosip
 
-# RRO
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Audio
 PRODUCT_PACKAGES += \
