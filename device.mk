f075eb4464b907b8c2359c6a6cb832897f46b597
dianlujitao <dianlujitao@lineageos.org>
2018-12-24 12:04:48 +0800

    lavender: Support Alipay fingerprint payment
     * IFAA manager is based on OnePlusOSS, but adapted for Xiaomi's mlipay
       interface.Also hardcode model detection to pass Alipay check.
     * vendor.xiaomi.hardware.mtdservice@1.0.so is not actually used, thus
       patchelf to drop it rather than shipping a blob.
     * Modify libmlipay.so to allow load firmware from vendor
    
    Change-Id: Idf3d3a8d40245984767f4ef5f60f9fe584e69f21

--- device.mk
+++ device.mk
@@ -155,6 +155,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -22,6 +22,7 @@
 #include <hardware/fingerprint.h>
 #include "BiometricsFingerprint.h"
 
+#include <cutils/properties.h>
 #include <inttypes.h>
 #include <unistd.h>
 
@@ -204,6 +205,10 @@ Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId,
     return ErrorFilter(mDevice->authenticate(mDevice, operationId, gid));
 }
 
+void setFpVendorProp(const char *fp_vendor) {
+    property_set("persist.sys.fp.vendor", fp_vendor);
+}
+
 IBiometricsFingerprint* BiometricsFingerprint::getInstance() {
     if (!sInstance) {
       sInstance = new BiometricsFingerprint();
@@ -263,6 +268,7 @@ fingerprint_device_t* getFingerprintDevice()
     if (fp_device == nullptr) {
         ALOGE("Failed to load fpc fingerprint module");
     } else {
+        setFpVendorProp("fpc");
         return fp_device;
     }
 
@@ -270,9 +276,11 @@ fingerprint_device_t* getFingerprintDevice()
     if (fp_device == nullptr) {
         ALOGE("Failed to load goodix fingerprint module");
     } else {
+        setFpVendorProp("goodix");
         return fp_device;
     }
 
+    setFpVendorProp("none");
     return nullptr;
 }
 
