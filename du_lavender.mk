916464cca0215cedb99a046a490d4f66504498a5
LuK1337 <priv.luk@gmail.com>
2019-09-12 23:57:18 +0200

    lavender: Override PRODUCT_DEVICE instead of TARGET_DEVICE
    * TARGET_DEVICE isn't really used for generating props
      anymore as tools/buildinfo_common.sh is using
      PRODUCT_DEVICE instead.
    
    Change-Id: Ic042b209a3af5d611d3a460a9add5f9e6e475918

--- du_lavender.mk
+++ du_lavender.mk
@@ -26,8 +26,8 @@ PRODUCT_MODEL := Redmi Note 7
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="lavender" \
-    PRODUCT_NAME="lavender" \
+    PRODUCT_DEVICE=lavender \
+    PRODUCT_NAME=lavender \
     PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 20.3.19 release-key"
 
 BUILD_FINGERPRINT := "google/crosshatch/crosshatch:10/QQ2A.200405.005/6254899:user/release-keys"
