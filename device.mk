be9370f21db89ce55837d04df3bb91059411e4bd
Subhajeet Muhuri <kenny3fcb@gmail.com>
2020-01-21 12:25:17 +0530

    lavender: Enable Pixel power HAL
    Change-Id: Ifcc7d2daa24b058079e050beac4867c6e57fc883
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- device.mk
+++ device.mk
@@ -335,7 +335,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.3-service.pixel-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
