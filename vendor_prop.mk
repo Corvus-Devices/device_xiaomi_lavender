6235732bd22339ba15e57df1a82faa87fd3400e5
dianlujitao <dianlujitao@lineageos.org>
2020-01-31 00:09:48 +0700

    lavender: Force enable mobile network settings v2
     * AOSP defaults to true but 10 QC telephony extension sets it to false.
     * Restore AOSP behavior by enabling it with a system property which has
       higher priority over global system settings which is used by QCOM w/o
       modifying the jar.
    
    Change-Id: I45854b91bb8cfafabfe811777cda9f1e1840febf
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -153,8 +153,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.data_ltd_sys_ind=1 \
     persist.vendor.radio.data_con_rprt=1 \
     persist.radio.calls.on.ims=1 \
+    persist.sys.fflag.override.settings_network_and_internet_v2=true \
     ro.telephony.iwlan_operation_mode=legacy \
-    ro.telephony.use_old_mnc_mcc_format=true 
+    ro.telephony.use_old_mnc_mcc_format=true
 
 
 # Sensor
