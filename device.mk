8b16f5925c0ce5a0b5d84e56ecfc1107090342fa
dianlujitao <dianlujitao@lineageos.org>
2020-02-02 19:33:37 +0800

    lavender: Explictly build camera HIDL interfaces
     * The 64-bit variants are used by IMS blobs but no longer automatically
       built along with the 32-bit counterparts.
    
    Change-Id: I387e4345c3088bbaf55e24bcfe286ccd877507a6
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- device.mk
+++ device.mk
@@ -106,8 +106,10 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
+    android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
     Snap
 
