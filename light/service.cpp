d9d72485c54ed1e129ba451cebe70bce9552a5e3
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-04 00:04:10 +0700

    lavender: Import lights hal from https://github.com/xiaomi-sdm660-devs/android_device_xiaomi_sdm660-common
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- light/service.cpp
+++ light/service.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The LineageOS Project
+ * Copyright 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@ using android::sp;
 using android::status_t;
 
 int main() {
-    sp<ILight> service = new Light();
+    android::sp<ILight> service = new Light();
 
     configureRpcThreadpool(1, true);
 
