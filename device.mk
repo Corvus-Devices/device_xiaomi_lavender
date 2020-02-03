8f10ef62619082dc7ed30d369d78c4a28b92ae89
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-04 04:04:33 +0700

    lavender: update blobs, ramdisk from LA.UM.8.2.r1-05100-sdm660.0 *also pick up ramdisk from https://github.com/xiaomi-sdm660-devs/android_device_xiaomi_sdm660-common/tree/jason/lineage-17.1
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- device.mk
+++ device.mk
@@ -310,6 +310,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Media Extensions
+PRODUCT_PACKAGES += \
+    libavmediaserviceextensions \
+    libmediametrics \
+    libregistermsext \
+    mediametrics
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
@@ -319,10 +326,12 @@ PRODUCT_PACKAGES += \
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
+    libextmedia_jni \
+    libhypv_intercept \
     libmm-omxcore \
+    libOmxCore \
     libOmxAacEnc \
     libOmxAmrEnc \
-    libOmxCore \
     libOmxEvrcEnc \
     libOmxG711Enc \
     libOmxQcelp13Enc \
@@ -352,12 +361,13 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
+    fstab.qcom \
     init.msm.usb.configfs.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
-    ueventd.qcom.rc \
+    init.recovery.qcom.rc \
     init.target.rc \
-    fstab.qcom
+    ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
