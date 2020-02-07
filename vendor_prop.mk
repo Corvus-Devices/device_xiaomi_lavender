89c7e148fa950e9f59a252060ea85a1d78ca3f44
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-07 20:11:17 +0700

    lavender: remove dalvik prop *already inherit in fw native
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -100,16 +100,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.media.codec2=2
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m \
-    ro.dalvik.vm.native.bridge=0
-
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
