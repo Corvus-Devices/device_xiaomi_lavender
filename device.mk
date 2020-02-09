3e31c46b1fc33dd8b0a635461d6d58ba6d2ea204
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-09 16:50:02 +0700

    lavender: remove overlay-aosip

--- device.mk
+++ device.mk
@@ -28,8 +28,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS := \
-    $(LOCAL_PATH)/overlay \
-#    $(LOCAL_PATH)/overlay-aosip
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
