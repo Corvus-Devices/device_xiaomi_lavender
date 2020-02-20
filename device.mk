4bc8a9293fb92a91d791f12a04add5f11a46c958
dianlujitao <dianlujitao@lineageos.org>
2020-02-20 11:42:40 +0700

    lavender: Update GPS from LA.UM.8.2.r1-05100-sdm660.0
    Change-Id: Ic615602598107c2ac1f629340547bc90f5f4c34a

--- device.mk
+++ device.mk
@@ -192,15 +192,27 @@ PRODUCT_ODM_PROPERTIES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    libsensorndkbridge
+    apdr.conf \
+    flp.conf \
+    gps.conf \
+    izat.conf \
+    lowi.conf \
+    sap.conf \
+    xtwifi.conf
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
-    $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
-    $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
-    $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+PRODUCT_PACKAGES += \
+    libbatching \
+    libcurl \
+    libgeofencing \
+    libgnss \
+    libgps.utils \
+    liblocation_api \
+    libsensorndkbridge \
+    libvehiclenetwork-native
+
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.0-impl-qti \
+    android.hardware.gnss@2.0-service-qti
 
 # Healthd
 PRODUCT_PACKAGES += \
