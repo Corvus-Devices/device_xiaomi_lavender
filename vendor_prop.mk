963c64b26e61df06ce6252a4da705adebfef58a2
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-23 21:42:49 +0700

    Revert "lavender: move all system.prop for camera to vendor_prop"
    This reverts commit a3334fd9db69ad20d5c27dc3aae8736da7233b01.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -140,18 +140,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.imx376_ofilm.low.lux=290 \
     persist.vendor.imx376_ofilm.light.lux=275 \
     persist.vendor.bokeh.switch.lux=290 \
-    persist.vendor.camera.exif.make=Xiaomi \
-    persist.vendor.camera.preview.ubwc=0 \
-    persist.vendor.camera.isp.clock.optmz=0 \
-    persist.vendor.camera.isp.turbo=1 \
-    persist.vendor.camera.imglib.usefdlite=1 \
-    persist.vendor.camera.expose.aux=1 \
-    persist.vendor.camera.HAL3.enabled=1 \
-    persist.vendor.camera.manufacturer=Xiaomi \
-    persist.vendor.camera.model=Redmi Note 7 \
-    persist.vendor.camera.privapp.list=org.codeaurora.snapcam \
-    persist.camera.privapp.list=org.codeaurora.snapcam \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam
+    persist.vendor.camera.exif.make=Xiaomi
 
 # Codec2 switch
 PRODUCT_PROPERTY_OVERRIDES += \
