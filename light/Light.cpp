5d62d23e7b54c2f619b7b6d6898f9b3806843dac
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-23 22:17:13 +0700

    Revert "lavender: light: Set saner ramp values"
    This reverts commit 55016394f4a89f28ba9b359452124abbec1c20cf.

--- light/Light.cpp
+++ light/Light.cpp
@@ -49,10 +49,10 @@ constexpr auto kDefaultMaxLedBrightness = 255;
 constexpr auto kDefaultMaxScreenBrightness = 4095;
 
 // Each step will stay on for 50ms by default.
-constexpr auto kRampStepDuration = 50;
+constexpr auto kRampStepDuration = 150;
 
 // Each value represents a duty percent (0 - 100) for the led pwm.
-constexpr std::array kBrightnessRamp = {0, 12, 25, 37, 50, 72, 85, 100};
+constexpr std::array kBrightnessRamp = {0, 12, 25, 37, 50, 72, 85, 100, 85, 72, 50, 37, 25, 12, 0};
 
 // Write value to path and close file.
 bool WriteToFile(const std::string& path, uint32_t content) {
