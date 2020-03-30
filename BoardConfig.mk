d7e6a3db9ee625fe2ba0a8033902e1663aaf8c8a
HarukeyUA <inazarrusnak@gmail.com>
2020-03-30 07:39:53 +0000

    lavender: Disable hashtree verification

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,7 @@ BUILD_BROKEN_PHONY_TARGETS := true
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 loop.max_part=7
